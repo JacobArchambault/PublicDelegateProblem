8b2789b8f09e843904946bb2119b8a637aa33d71
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-13 10:47:08 -0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublicDelegateProblem
+{
+    public class Car
+    {
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        // Now a public member!
+        public CarEngineHandler listOfHandlers;
+
+        // Just fire out the Exploded notification.
+        public void Accelerate(int delta)
+        {
+            listOfHandlers?.Invoke("Sorry, this car is dead...");
+        }
+
+    }
+}
